c96b6b040a94a489f0b0f29997a9739e60b39193
Mateusz <ryniuss@o2.pl>
2023-07-12 18:59:46 +0200

    EC-2: set globalStyle & theme

---
+++ src/styles/theme.tsx
@@ -0,0 +1,16 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+	powderWhite: '#FFFDF9',
+	persianGreen: '#06B49A',
+	lightBlue: '#AFDBD2',
+	onyx: '#36313D',
+};
+
+export const breakpoints = {
+	xxs: '(max-width: 480px)',
+	xs: '(max-width: 768px)',
+	sm: '(max-width: 1024px)',
+	md: '(max-width: 1280px)',
+	xl: '(max-width: 1660px)',
+};
