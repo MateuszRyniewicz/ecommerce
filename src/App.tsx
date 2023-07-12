9558caddf54b3131d7b21994f619fc83fa960a1f
Mateusz <ryniuss@o2.pl>
2023-07-12 19:00:36 +0200

    EC-2: create layout & router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './_shared/layout/Layout';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				//		element: <HomePage />,
+			},
+			{
+				path: '/dashboard',
+				//		element: <Dashboard />,
+			},
+		],
+	},
+]);
+
 const App = () => {
-	return <div>test</div>;
+	return (
+		<div className='app'>
+			<div className='container'>
+				<RouterProvider router={router} />
+			</div>
+		</div>
+	);
 };
 
 export default App;
